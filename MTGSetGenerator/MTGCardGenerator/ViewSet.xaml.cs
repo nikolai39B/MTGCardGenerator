71341ebf79da483a43ced63ffc3ca78c587d1c31
nikolai39B <nikolai39B@gmail.com>
2016-06-01 14:51:29 -0400

    Various additions.

---
+++ MTGSetGenerator/MTGCardGenerator/ViewSet.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MTGSetGenerator
+{
+    /// <summary>
+    /// Interaction logic for ViewSet.xaml
+    /// </summary>
+    public partial class ViewSet : UserControl
+    {
+        public ViewSet()
+        {
+            InitializeComponent();
+        }
+    }
+}
